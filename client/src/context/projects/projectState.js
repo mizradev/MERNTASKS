2a8d6aa4f2711c13c29ff97f1766adf0e6326614
soymizra <jorgemizrain@gmail.com>
2021-02-13 14:21:10 -0600

    ProjectState added

---
+++ client/src/context/projects/projectState.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import projectContext from './projectContext';
+import projectReducer from './projectReducer';
+
+const ProjectState = props => {
+    const initialState ={
+        form: false
+    }
+
+    //dispatch of actions
+    const [ state, dispatch ] = useReducer(projectReducer, initialState); 
+
+    // function for project
+
+    return (
+        <projectContext.Provider
+            value={{ form: state.form }}
+        >
+            {props.children}
+        </projectContext.Provider>
+    );
+}
+
+export default ProjectState;
